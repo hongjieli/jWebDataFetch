9b56cbb0047961e3256cf30a896380d61bc0677d
honli <honli@SHALHONLI1.amd.com>
2014-11-21 17:14:40 +0800

    using htmlunit to fetch data coming from a web page generating by AJAX

---
+++ jLiCaiWangDataFetch/src/jlicaiwangdatafetch/JLiCaiWangDataFetch.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jlicaiwangdatafetch;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import java.io.IOException;
+
+/**
+ *
+ * @author honli
+ */
+public class JLiCaiWangDataFetch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException, InterruptedException {
+        // TODO code application logic here
+        final WebClient webClient=new WebClient(BrowserVersion.CHROME);
+        webClient.setAjaxController(new NicelyResynchronizingAjaxController());
+        webClient.getOptions().setJavaScriptEnabled(true);
+        webClient.getOptions().setActiveXNative(false);
+        webClient.getOptions().setCssEnabled(false);
+        final HtmlPage page=webClient.getPage("http://www.chinawealth.com.cn/zzlc/jsp/lccp.jsp");
+        Thread.sleep(20000);
+        System.out.println(page.asText());
+        webClient.closeAllWindows();
+        
+    }
+    
+}
